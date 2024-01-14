8ce6743839c767e1d5e9e97ac2b611cd89818bd2
Gerardo <gerardoijgv11@gmail.com>
2024-01-13 20:27:08 -0600

    prueba

--- ConvertingData.py
+++ ConvertingData.py
@@ -3,4 +3,4 @@ yearOfBirth = input("Hola " + name + " , en qué año naciste?")
 
 age =2024- int(yearOfBirth)
 age = str(age)
-print("Te llamas " + name + " y tienes "+ age)
+print("Te llamas " + name + " y tienes "+ age + ". Prueba de que ya no se usa contraseña")
